8b63fc336a1e845e564d9bb4dcc33130fa4c8e4e
Woodney Juste <woodneydev@gmail.com>
2023-08-11 11:16:16 -0400

    get, post, delete, routes working

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,5 +1,10 @@
 const router = require("express").Router()
 const fs = require("fs");
+const crypto = require("crypto");
+
+const generateID = (length) => {
+    return crypto.randomBytes(length).toString("hex");
+}
 
 const readTodos = () => {
     let todos = fs.readFileSync("./data/todos.json")
@@ -9,13 +14,39 @@ const readTodos = () => {
 
 const pushNewTodo = (newObj) => {
     let todoList = readTodos();
+    console.log
     todoList.push(newObj)
-    fs.writeFileSync("./data/todos.json", JSON.stringify(newObj))
+    fs.writeFileSync("./data/todos.json", JSON.stringify(todoList))
 }
 
+const overideTodoList = (newList) => {
+    fs.writeFileSync("./data/todos.json", JSON.stringify(newList))
+}
 
-const postTodos = (req, res) => {
-    const {data} = req.body
+const validatePost = (data) => {
+    if (data.todo === "" || typeof data.checked !== "boolean") {
+        return false
+    } else {
+        return true
+    }
+}
+
+const doesExist = (id) => {
+    const todoList = readTodos();
+    const exist = todoList.find(item => item.id === id)
+    if (exist) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const deleteTodo = (id) => {
+    const todoList = readTodos();
+
+    const newTodoList = todoList.filter(item => item.id !== id)
+
+    return newTodoList
 }
 
 router.get("/", (req, res) => {
@@ -23,7 +54,30 @@ router.get("/", (req, res) => {
     res.status(200).json(todoList)
 })
 .post("/", (req, res) => {
+    const {data} = req.body
+    console.log("incoming request post", data)
+    const isValidPost = validatePost(data)
 
+    if(isValidPost) {
+        const postObj = {
+            ...data,
+            id: `${generateID(4)}`
+        }
+
+        pushNewTodo(postObj)
+        res.status(201).json({data: postObj})
+    } else {
+        res.status(400).json({data: {error: "please send valid fields"}})
+    }
 })
-
+.delete("/:id", (req, res) => {
+    const {id} = req.params
+    if (doesExist(id)) {
+        const newList = deleteTodo(id)
+        overideTodoList(newList)
+        res.status(200).json({data: "Resource was deleted"})
+    } else {
+        res.status(400).json({data: {error: "Item does not exist"}})
+    }
+})
 module.exports = router
